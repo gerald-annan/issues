a89b0034049ae78cae9bdc35d9453e1739dcd33b
gerald-annan <geraldannan400@gmail.com>
2021-12-18 16:54:27 +0000

    extracting the first n items from list

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -51,9 +51,17 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 
   def decode_response({:ok, body}), do: body
