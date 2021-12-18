8bf93356385dec9cdb274c249d15dc85f3f5bb05
gerald-annan <geraldannan400@gmail.com>
2021-12-18 13:03:10 +0000

    Processing user data and project into data structure containing Github issues with httpoison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -51,7 +51,7 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  # def process({user, project, _count}) do
-  #   Issues.GithubIssues.fetch(user, project)
-  # end
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
 end
