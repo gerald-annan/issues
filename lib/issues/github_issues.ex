f58ef46fd359bc718ea5f85af75ad8ff8ce9a9ad
gerald-annan <geraldannan400@gmail.com>
2021-12-30 12:25:10 +0000

    Adds some logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,7 +1,34 @@
 defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
   @github_url Application.get_env(:issues, :github_url)
+  require Logger
+
+  @spec fetch(any, any) ::
+          {:error,
+           false
+           | nil
+           | true
+           | binary
+           | [false | nil | true | binary | list | number | {any, any, any} | map]
+           | number
+           | %{
+               optional(atom | binary | {any, any, any}) =>
+                 false | nil | true | binary | list | number | {any, any, any} | map
+             }}
+          | {:ok,
+             false
+             | nil
+             | true
+             | binary
+             | [false | nil | true | binary | list | number | {any, any, any} | map]
+             | number
+             | %{
+                 optional(atom | binary | {any, any, any}) =>
+                   false | nil | true | binary | list | number | {any, any, any} | map
+               }}
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -12,6 +39,8 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+
     {
       status_code |> check_for_error(),
       body |> Poison.Parser.parse!()
